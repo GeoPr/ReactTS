2823a0c03ee0fac8a6c80c91e9bcfee257ae9b53
George <klassnoyparen@gmail.com>
2020-10-29 19:40:37 +0300

    commit

--- src/assets/components/App.tsx
+++ src/assets/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <div className="wrapper">
       <>it works </>
--- src/assets/state/actions.ts
+++ src/assets/state/actions.ts
@@ -1,19 +1,4 @@
-import React from 'react';
 import { app_TEST_CASE } from './types';
-
-interface IDispatch {
-  type: string;
-  payload?: object;
-}
 
 // app
 export const app_testCase = (test: any) => ({ type: app_TEST_CASE, payload: { test } });
-
-export const asyncActionCreator = (
-  dispatch: React.Dispatch<React.SetStateAction<IDispatch>>,
-  fn: () => IDispatch,
-): any => {
-  return () => {
-    return dispatch(fn());
-  };
-};
